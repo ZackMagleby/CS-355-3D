e15ff1e96ab3b346bb9618bb7d1091447b31fdcc
PMPC-AD\maglebyz <zackmagleby@yahoo.com>
2017-05-30 15:53:04 -0600

    initial commit

---
+++ src/CS355/LWJGL/Solution.java
@@ -0,0 +1,29 @@
+package CS355.LWJGL;
+
+public class Solution implements CS355LWJGLController {
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resizeGL() {
+		//Change
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateKeyboard() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
