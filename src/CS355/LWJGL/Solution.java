5d568cf82c708704fd6c8031b2e09df984127447
ZackMagleby <zackmagleby@yahoo.com>
2017-05-31 11:15:26 -0600

    Starting out

--- src/CS355/LWJGL/Solution.java
+++ src/CS355/LWJGL/Solution.java
@@ -4,25 +4,33 @@ public class Solution implements CS355LWJGLController {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		//matrixMode(GL_PRojectionMatrix)
+		//loadIdentity
+		//rotate
+		//translate
+		
+		//glOrtho vs gluPerspective
 
 	}
 
 	@Override
 	public void resizeGL() {
-		//Change
+		//Initialize ModelView
+		//Initialize Projection
 
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-
+		//update data as needed
 	}
 
 	@Override
 	public void updateKeyboard() {
-		// TODO Auto-generated method stub
+		//poll keyboard
+			//if movement, update x,y,z
+			//if turn, update rotation
+			//if o or p, update mode
 
 	}
 
