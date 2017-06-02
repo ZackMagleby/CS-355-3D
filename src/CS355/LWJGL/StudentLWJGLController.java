1e346cb073b7fafbfb3f564f6e373dd0db8acebe
ZackMagleby <zackmagleby@yahoo.com>
2017-06-01 18:52:42 -0600

    Almost there

--- src/CS355/LWJGL/StudentLWJGLController.java
+++ src/CS355/LWJGL/StudentLWJGLController.java
@@ -11,6 +11,9 @@ package CS355.LWJGL;
 //Of course, your milage may vary. Don't feel restricted by this list of imports.
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLUConstants;
+
+import sun.text.resources.es.FormatData_es_SV;
 
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_LINES;
@@ -23,11 +26,13 @@ import static org.lwjgl.opengl.GL11.glEnd;//
 import static org.lwjgl.opengl.GL11.glLoadIdentity;//
 import static org.lwjgl.opengl.GL11.glMatrixMode;//
 import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glRotatef;//
-import static org.lwjgl.opengl.GL11.glTranslatef;//
+import static org.lwjgl.opengl.GL11.glRotated;//
+import static org.lwjgl.opengl.GL11.glTranslated;//
 import static org.lwjgl.opengl.GL11.glVertex3d;//
 import static org.lwjgl.opengl.GL11.glViewport;//
 import static org.lwjgl.opengl.GL11.glOrtho;
+import org.lwjgl.util.glu.GLU;
+
 
 import java.util.Iterator;
 
@@ -46,20 +51,24 @@ public class StudentLWJGLController implements CS355LWJGLController
   //If not, I apologize.
   private WireFrame model = new HouseModel();
   
-  Point3D cameraPos = new Point3D(-.1, -2.5,-5);
+  Point3D cameraPos = new Point3D(0, -2.5, -10);
   //Point3D cameraPos = new Point3D(0, 0, 0);
   Double rotation = 0.0;
-  int mode = 0;
+  int mode = 1;
+  int farPlane = 1000;
+  int nearPlane = 0;
+  final double fov = 90.0;
+  LWJGLSandbox sandbox = new LWJGLSandbox();
 
   //This method is called to "resize" the viewport to match the screen.
   //When you first start, have it be in perspective mode.
   @Override
   public void resizeGL() 
   {
-	  GL11.glViewport(0, 0, 640, 480);
-	  GL11.glMatrixMode(GL_PROJECTION);
-		//Initialize ModelView
-		//Initialize Projection
+	  glMatrixMode(GL_PROJECTION);
+	  glViewport((int)cameraPos.x, (int)cameraPos.y, sandbox.DISPLAY_WIDTH, sandbox.DISPLAY_HEIGHT);
+	  mode = 1;
+	  GLU.gluPerspective((float)fov, (float)4/3, (float)nearPlane, (float)farPlane);
   }
 
     @Override
@@ -77,52 +86,52 @@ public class StudentLWJGLController implements CS355LWJGLController
     {
         if(Keyboard.isKeyDown(Keyboard.KEY_W)) 
         {
-            System.out.println("You are pressing W!");
+            //System.out.println("You are pressing W!");
             cameraPos.z += .1; 
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_S)) 
         {
-            System.out.println("You are pressing S!");
+            //System.out.println("You are pressing S!");
             cameraPos.z -= .1; 
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_A)) 
         {
-            System.out.println("You are pressing A!");
+            //System.out.println("You are pressing A!");
             cameraPos.x += .1; 
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_D)) 
         {
-            System.out.println("You are pressing D!");
+            //System.out.println("You are pressing D!");
             cameraPos.x -= .1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_Q)) 
         {
-            System.out.println("You are pressing Q!");
+            //System.out.println("You are pressing Q!");
             rotation -= 1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_E)) 
         {
-            System.out.println("You are pressing E!");
+           // System.out.println("You are pressing E!");
             rotation += 1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_R)) 
         {
-            System.out.println("You are pressing R!");
+            //System.out.println("You are pressing R!");
             cameraPos.y -= .1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_F)) 
         {
-            System.out.println("You are pressing F!");
+            //System.out.println("You are pressing F!");
             cameraPos.y += .1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_P)) 
         {
-            System.out.println("You are pressing P!");
+           // System.out.println("You are pressing P!");
             mode = 1;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_O)) 
         {
-            System.out.println("You are pressing O!");
+           //System.out.println("You are pressing O!");
             mode = 0;
         }
         else if(Keyboard.isKeyDown(Keyboard.KEY_H)) 
@@ -138,48 +147,48 @@ public class StudentLWJGLController implements CS355LWJGLController
         //This clears the screen.
         glClear(GL_COLOR_BUFFER_BIT);
         setMatrices();
-        //drawLines();
+    }
+    
+    
+    public void setMatrices(){
+    	
+    	glMatrixMode(GL_PROJECTION);
+    	glLoadIdentity();  
+
+    	if(mode == 0){
+    		glOrtho(-30, 30, -30, 30, nearPlane, farPlane);
+    	}
+    	else{
+    		GLU.gluPerspective((float)fov, (float)4/3, (float)nearPlane, (float)farPlane);
+    	}
+    	
+    	glMatrixMode(GL_MODELVIEW);
+    	glLoadIdentity();  
+    	glRotated(rotation, 0, 1, 0);
+    	glTranslated(cameraPos.x, cameraPos.y, cameraPos.z);
+    	glPushMatrix();
+    	
+    	drawLines();
+    	
+    	GL11.glPopMatrix();
+    	
+    	glMatrixMode(GL_PROJECTION);
     }
     
     public void drawLines(){
     	//Do your drawing here.
-        Iterator<Line3D> lines = model.getLines();
-        while(lines.hasNext()){
-        	Line3D line = lines.next();
-        	Point3D start = line.start;
-        	Point3D end = line.end;
-        	
-        	GL11.glColor3f(0.0f, 1.0f, 0.2f);
-            GL11.glBegin(GL_LINES);
-
-            GL11.glVertex3d(start.x, start.y, start.z);
-            GL11.glVertex3d(end.x, end.y, end.z);
-            GL11.glEnd();
-        }
+    	Iterator<Line3D> lines = model.getLines();
+    	glBegin(GL_LINES);
+    	while(lines.hasNext()){
+    		Line3D line = lines.next();
+    		Point3D start = line.start;
+    		Point3D end = line.end;
+    		
+    		glColor3f(0.0f, 1.0f, 0.2f);
+    		
+    		glVertex3d(start.x, start.y, start.z);
+    		glVertex3d(end.x, end.y, end.z);
+    	}
+    	glEnd();
     }
-    
-    public void setMatrices(){
-    	int f = 100;
-    	int n = 1;
-    	double clipScale = (f+n)/(f-n);
-    	double otherScale = (-2*n*f)/(f-n);
-    	GL11.glMatrixMode(GL_PROJECTION);
-
-    	GL11.glMatrixMode(GL_MODELVIEW);
-    	GL11.glLoadIdentity();
-    	GL11.glRotated(rotation, 0, cameraPos.y, 0);
-    	GL11.glTranslated(cameraPos.x, cameraPos.y, cameraPos.z);
-    	drawLines();
-    	
-    	GL11.glMatrixMode(GL_PROJECTION);
-    	//GL11.glRotated(-rotation, 0, -cameraPos.y, 0); 	
-    	//GL11.glTranslated(-cameraPos.x, -cameraPos.y, -cameraPos.z);
-		//matrixMode(GL_PRojectionMatrix)
-		//loadIdentity
-		//rotate
-		//translate
-		
-		//glOrtho vs gluPerspective
-    }
-    
 }
